67b0e1e5f4957917ac9e70c48f0cdf51c2118e47
if1live <libsora25@gmail.com>
2015-10-10 04:39:15 -0400

    스택 지역 변수의 주소값을 이용한 버그 v2

--- content/development/stack-allocation-voodoo-magic/inf_loop_bug.c
+++ content/development/stack-allocation-voodoo-magic/inf_loop_bug.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
-int g_loop_count = 0;
+static int g_loop_count = 0;
 
-const int MAX_LOOP_A = 2;
-void run_loop_a()
+#define MAX_LOOP_A 2
+static void run_loop_a()
 {
 	int i = 1234;
 	int data[MAX_LOOP_A];
 
 	g_loop_count = 0;
-	printf("addr i    : %lx\n", (unsigned long)&i);
-	printf("addr data: %lx\n", (unsigned long)data);
+	printf("addr i\t\t: %lx\n", (unsigned long)&i);
+	printf("addr data\t: %lx\n", (unsigned long)data);
 
 	for(i = 0 ; i <= MAX_LOOP_A ; ++i) {
+		printf("curr addr[%d]\t: %lx\n", i, (unsigned long)&data[i]);
 		data[i] = 0;
-		printf("curr addr : %lx\n", (unsigned long)&data[i]);
 
 		g_loop_count++;
 		if(g_loop_count > MAX_LOOP_A + 10) {
@@ -24,19 +24,19 @@ void run_loop_a()
 	}
 }
 
-const int MAX_LOOP_B = 4;
-void run_loop_b()
+#define MAX_LOOP_B 4
+static void run_loop_b()
 {
 	int i = 1234;
 	int data[MAX_LOOP_B];
 
 	g_loop_count = 0;
-	printf("addr i    : %lx\n", (unsigned long)&i);
-	printf("addr data: %lx\n", (unsigned long)data);
+	printf("addr i\t\t: %lx\n", (unsigned long)&i);
+	printf("addr data\t: %lx\n", (unsigned long)data);
 
 	for(i = 0 ; i <= MAX_LOOP_B ; ++i) {
+		printf("curr addr[%d]\t: %lx\n", i, (unsigned long)&data[i]);
 		data[i] = 0;
-		printf("curr addr : %lx\n", (unsigned long)&data[i]);
 
 		g_loop_count++;
 		if(g_loop_count > MAX_LOOP_B + 10) {
