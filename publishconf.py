b578b457f247d8088b9b9e156cac96a8a3f127eb
if1live <libsora25@gmail.com>
2016-02-13 21:34:44 +0900

    http/https 삽질관련 변수 일단 제거

--- publishconf.py
+++ publishconf.py
@@ -10,8 +10,8 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-#SITEURL = ''
-SITEURL = 'https://libsora.so'
+SITEURL = ''
+#SITEURL = 'https://libsora.so'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
