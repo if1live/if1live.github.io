b0c21f2d4450bed8e3f4c6e73f5798a2e0d3d490
if1live <libsora25@gmail.com>
2016-03-06 18:59:43 +0900

    printf("%s\n", NULL)의 동작

---
+++ content/post-mortem/printf-with-null-post-mortem/printf_prec.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+  printf("%s\n", NULL);
+  printf("%.0s\n", NULL);
+  printf("%.3s\n", NULL);
+  return 0;
+}
---
+++ content/post-mortem/printf-with-null-post-mortem/simple.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+  printf("string = %s\n", NULL);
+  return 0;
+}
