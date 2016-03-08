774f1b20cc0ef1226713092e686061eb01aa4076
if1live <libsora25@gmail.com>
2016-03-08 23:06:24 +0900

    hugo용 마크다운 만들떄 isCJKLanguage 속성 끼워넣기

--- bin/build_all_article.sh
+++ bin/build_all_article.sh
@@ -5,6 +5,7 @@
 
 mode=$1
 
+pids=()
 for filepath in $(find ./content -name "*.mkdn"); do
 	category=$(echo $filepath | awk -F "/" '{print $3}')
 	dirname=$(echo $filepath | awk -F "/" '{print $4}')
@@ -12,6 +13,19 @@ for filepath in $(find ./content -name "*.mkdn"); do
 	echo "$category : $dirname : $filename"
 
 	cd ./content/$category/$dirname
-	maya -mode=$mode -file=$filename > ../$dirname.md
+	maya -mode=$mode -file=$filename > ../$dirname.md &
+	pids+=($!)
 	cd -
+done
+for pid in ${pids[@]}; do
+	wait $pid
+done
+
+# post-process - for hugo
+for target in $(find ./content -name "*.md"); do
+	has_cjk_lang=$(grep "isCJKLanguage = true" $target|wc -l)
+	if [[ $has_cjk_lang == "1" ]]; then
+		line=$(grep -n "^+++$" $target|sed -n 2p|awk -F ":" '{print $1}')
+		ed -s $target <<< $line$'i\nisCJKLanguage = true\n.\nwq'
+	fi
 done
