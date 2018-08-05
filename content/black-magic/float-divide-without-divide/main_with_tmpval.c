8c87836fe059a177a820333c222292f2abd6ceda
if1live <libsora25@gmail.com>
2018-08-05 12:53:31 +0900

    나눗셈 연산을 곱셈으로 프로그래밍 할 수 있음?

---
+++ content/black-magic/float-divide-without-divide/main_with_tmpval.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <math.h>
+
+float Q_rsqrt( float number )
+{
+  long i;
+  float x2, y;
+  const float threehalfs = 1.5F;
+
+  x2 = number * 0.5F;
+  y  = number;
+  i  = * ( long * ) &y;                       // evil floating point bit level hacking
+  i  = 0x5f3759df - ( i >> 1 );               // what the fuck?
+  y  = * ( float * ) &i;
+  y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+//y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration, this can be removed
+
+  return y;
+}
+
+void print(float n) {
+  float result_1 = 1 / n;
+
+  float x = Q_rsqrt(n);
+  float result_2 = x * x;
+  printf("%f %f\n", result_1, result_2);
+}
+
+int main() {
+  print(1000.0f);
+  print(100.0f);
+  print(10.0f);
+  print(1.0f);
+  print(0.1f);
+  print(0.01f);
+  return 0;
+}
