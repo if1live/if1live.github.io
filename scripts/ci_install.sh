34028c357fc01786bded82998587188fcdbaa788
if1live <libsora25@gmail.com>
2019-09-21 21:43:01 +0900

    설치 스크립트 통합

---
+++ scripts/ci_install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -ex
+
+# dep
+sudo wget -O /usr/local/bin/dep https://github.com/golang/dep/releases/download/v0.5.4/dep-linux-amd64
+sudo chmod 755 /usr/local/bin/dep
+
+# wt
+wget -O /tmp/wt.tar.gz https://github.com/wellington/wellington/releases/download/v1.0.4/wt_v1.0.4_linux_amd64.tar.gz
+sudo tar xvfz /tmp/wt.tar.gz -C /usr/local/bin
+
+# hugo
+wget -O /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.58.3/hugo_0.58.3_Linux-64bit.deb
+sudo dpkg -i /tmp/hugo.deb
--- scripts/ci_install_hugo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-wget -O /tmp/hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.58.3/hugo_0.58.3_Linux-64bit.deb
-sudo dpkg -i /tmp/hugo.deb
