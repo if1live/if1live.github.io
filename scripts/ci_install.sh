0c948df52daf0f1d13523597fab7c245a3b58e23
if1live <libsora25@gmail.com>
2020-11-13 14:02:00 +0900

    update build tool

--- scripts/ci_install.sh
+++ scripts/ci_install.sh
@@ -3,9 +3,9 @@
 set -ex
 
 # wt
-wget -O /tmp/wt.tar.gz https://github.com/wellington/wellington/releases/download/v1.0.4/wt_v1.0.4_linux_amd64.tar.gz
+wget -O /tmp/wt.tar.gz https://github.com/wellington/wellington/releases/download/v1.0.5/wt_v1.0.5_linux_amd64.tar.gz
 sudo tar xvfz /tmp/wt.tar.gz -C /usr/local/bin
 
 # hugo
-wget -O /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.58.3/hugo_0.58.3_Linux-64bit.tar.gz
+wget -O /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.78.1/hugo_extended_0.78.1_Linux-64bit.tar.gz
 sudo tar xvfz /tmp/hugo.tar.gz -C /usr/local/bin
