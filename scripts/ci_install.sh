51bf3e4749dc86dac571a13bcec54906877815b2
if1live <libsora25@gmail.com>
2021-08-01 21:33:10 +0900

    update hugo

--- scripts/ci_install.sh
+++ scripts/ci_install.sh
@@ -7,5 +7,5 @@ wget -O /tmp/wt.tar.gz https://github.com/wellington/wellington/releases/downloa
 sudo tar xvfz /tmp/wt.tar.gz -C /usr/local/bin
 
 # hugo
-wget -O /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.78.1/hugo_extended_0.78.1_Linux-64bit.tar.gz
+wget -O /tmp/hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v0.86.1/hugo_extended_0.86.1_Linux-64bit.tar.gz
 sudo tar xvfz /tmp/hugo.tar.gz -C /usr/local/bin
