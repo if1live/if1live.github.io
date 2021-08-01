49a6a4d4a5ca07f36dd4defaa433fcb8fe589add
if1live <libsora25@gmail.com>
2021-08-01 22:55:36 +0900

    serverless-webpack 깨지는 예제

---
+++ content/serverless/deploying-serverless-3-flaw-of-serverless-webpack/src/handler.ts
@@ -0,0 +1,21 @@
+import {
+  APIGatewayProxyHandlerV2,
+} from 'aws-lambda';
+
+import 'aws-sdk';
+import 'dynamo-types';
+import 'lodash';
+import 'typeorm';
+import 'mysql';
+
+import 'aws-xray-sdk-core';
+
+export const hello: APIGatewayProxyHandlerV2 = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }, null, 2),
+  };
+};
